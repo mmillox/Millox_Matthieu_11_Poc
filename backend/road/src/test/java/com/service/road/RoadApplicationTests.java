26a6508f27a974db28def999251b5927ca595933
mmillox <57156866+mmillox@users.noreply.github.com>
2024-08-29 17:57:21 +0200

    Update

--- backend/road/src/test/java/com/service/road/RoadApplicationTests.java
+++ backend/road/src/test/java/com/service/road/RoadApplicationTests.java
@@ -1,13 +1,35 @@
 package com.service.road;
 
+import com.service.road.controllers.RoadController;
+import com.service.road.services.RouteService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-class RoadApplicationTests {
+public class RoadApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Mock
+    private RouteService routeService;
 
+    @InjectMocks
+    private RoadController roadController;
+
+    @Test
+    void testGetRoute() {
+        String mockRoute = "Sample Route Data";
+        
+        when(routeService.getRoute(any(String.class), any(String.class))).thenReturn(mockRoute);
+        
+        ResponseEntity<?> response = roadController.getRoute("pointA", "pointB");
+        
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(mockRoute, response.getBody());
+    }
 }
